e56a4ef3d9c72b6c03da276c2eb760a4780f4a10
ivanchulde <aniv.ing.ec@gmail.com>
2025-05-09 23:11:18 -0500

    Finish exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        List<int> positive_numbers = new List<int>();
+        int new_number = -1;
+        float total = 0;
+        
+        Console.WriteLine("Entet a list of numbers, type 0 when finished.");
+
+        while (new_number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            new_number = int.Parse(input);
+            if (new_number != 0)
+            {
+                numbers.Add(new_number);
+            }
+            if (new_number > 0)
+            {
+                positive_numbers.Add(new_number);
+            }
+        }
+        Console.WriteLine("The numbers are: ");
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        float average = ((float)total) / numbers.Count;
+        int max_number = numbers.Max();
+        int min_number = numbers.Min();
+        numbers.Sort();
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max_number}");
+        Console.WriteLine($"The smallest positive number is: {min_number}");
+        Console.WriteLine("The sorted list of numbers is: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
