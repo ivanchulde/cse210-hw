6d1b4e3c5f7ac351194e4d4fd2f7ad8d38ab5afe
ivanchulde <aniv.ing.ec@gmail.com>
2025-05-23 13:10:30 -0500

    W03 Learning Activity: Encapsulation DONE

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(1, 2);
+        f1.SetBottom(3);
+        Console.WriteLine($"f1: {f1.GetFractionString()}");
+        Console.WriteLine($"f1: {f1.GetDecimalValue()}");
+        Console.WriteLine($"f2: {f2.GetFractionString()}");
+        Console.WriteLine($"f2: {f2.GetDecimalValue()}");
+        Console.WriteLine($"f3: {f3.GetFractionString()}");
+        Console.WriteLine($"f3: {f3.GetDecimalValue()}");
     }
 }
